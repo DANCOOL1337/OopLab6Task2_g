d62a542dcc3339af69df0d64793ca415099919f9
DANCOOL1337 <kozind311@gmail.com>
2026-04-17 09:41:51 +0500

    Update LineSegment.cs

--- OopLab6Task2/LineSegment.cs
+++ OopLab6Task2/LineSegment.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 
 namespace OopLab6
 {
     class LineSegment
     {
-        // Поля (Задание 2)
+        
         private double x;
         private double y;
 
-        // Свойства без автосвойств (Задание 2)
+        
         public double X
         {
             get { return x; }
@@ -21,7 +21,7 @@ namespace OopLab6
             set { y = value; }
         }
 
-        // Конструкторы (Задание 2)
+        
         public LineSegment()
         {
             this.x = 0;
@@ -34,7 +34,7 @@ namespace OopLab6
             this.y = y;
         }
 
-        // Метод пересечения (Задание 2)
+        
         public LineSegment Intersect(LineSegment other)
         {
             if (other == null) return null;
@@ -55,9 +55,9 @@ namespace OopLab6
             return null;
         }
 
-        // --- Перегрузка операций (Задание 3) ---
+        // --- (Задание 3) ---
 
-        // Унарная операция ! (исправление диапазона через 0)
+        
         public static LineSegment operator !(LineSegment s)
         {
             double newX = s.x;
@@ -65,44 +65,43 @@ namespace OopLab6
 
             if (newX > newY)
             {
-                // Пытаемся занулить X.
-                // Если 0 <= Y, диапазон станет правильным.
+               
 
                 if (0 <= newY) newX = 0;
-                // Иначе пытаемся занулить Y.
-                // Если X <= 0, диапазон станет правильным.
+                
                 else if (newX <= 0) newY = 0;
-                // Если оба варианта не спасают, зануляем оба.
+                
                 else { newX = 0; newY = 0; }
             }
             return new LineSegment(newX, newY);
         }
 
-        // Неявное приведение к int (целая часть Y)
+        
         public static implicit operator int(LineSegment s)
         {
             return (int)s.y;
         }
 
-        // Явное приведение к double (координата X)
+        
         public static explicit operator double(LineSegment s)
         {
             return s.x;
         }
 
-        // Бинарная операция + (левосторонняя:
-        // отрезок + число = меняем X)
-        public static LineSegment operator +(LineSegment s, int val)
+        
+        public static LineSegment operator +(LineSegment s, 
+            int val)
         {
             LineSegment res = new LineSegment(s.x + val, s.y);
-            // Обеспечиваем правильный диапазон
-            if (res.x > res.y) { double t = res.x; res.x = res.y; res.y = t; }
+            
+            if (res.x > res.y) { double t = res.x; res.x = 
+                    res.y; res.y = t; }
             return res;
         }
 
-        // Бинарная операция + (правосторонняя:
-        // число + отрезок = меняем Y)
-        public static LineSegment operator +(int val, LineSegment s)
+        
+        public static LineSegment operator +(int val,
+            LineSegment s)
         {
             LineSegment res = new LineSegment(s.x, s.y + val);
             if (res.x > res.y) 
@@ -110,8 +109,9 @@ namespace OopLab6
             return res;
         }
 
-        // Оператор > (левый включает правый)
-        public static bool operator >(LineSegment left, LineSegment right)
+        
+        public static bool operator >(LineSegment left,
+            LineSegment right)
         {
             double lMin = Math.Min(left.x, left.y);
             double lMax = Math.Max(left.x, left.y);
@@ -120,8 +120,9 @@ namespace OopLab6
             return (lMin <= rMin) && (lMax >= rMax);
         }
 
-        // Обязательная пара для >
-        public static bool operator <(LineSegment left, LineSegment right)
+        
+        public static bool operator <(LineSegment left,
+            LineSegment right)
         {
             return !(left > right);
         }
